9e1f8859ebed82653042ae782df8376292c92f1a
Lasa2 <hi.lasse11@gmail.com>
2021-04-16 23:55:25 +0200

    Rework of the Ui, Addded a Settings Window (#18)
    * init
    
    * working settings window with tray
    
    * renaming things, init journalfileapp
    
    * first full working app
    
    * sort import
    
    * clean
    
    * fixes
    
    * refactor
    
    * Autoclose
    
    * autoexit
    
    * fixes
    
    * mainmenu iprovments
    
    * fix crash on cqc
    
    * updated req

---
+++ JournalFileApp.py
@@ -0,0 +1,116 @@
+import json
+import msvcrt
+import os
+import re
+import time
+from queue import Queue
+from typing import Dict, Tuple
+
+import win32file
+import win32gui
+
+
+def getLauncher():
+    if win32gui.FindWindow(None, "Elite Dangerous Launcher"):
+        return True
+    else:
+        return False
+
+
+def getGame():
+    if win32gui.FindWindow(None, "Elite - Dangerous (CLIENT)"):
+        return True
+    else:
+        return False
+
+
+class JournalFileApp:
+    active_file: Tuple[int, os.DirEntry] = (0, None)
+    queue = Queue()
+    running = True
+    new_file = False
+    stop = False
+    last_msg = dict()
+
+    def __init__(self, con, journalpath) -> None:
+        self.con = con
+        self.journalpath = journalpath
+
+    def get_journal_file(self):
+        with os.scandir(self.journalpath) as it:
+            for entry in it:
+                if entry.is_file() and "Journal" in entry.name and ".log" in entry.name:
+                    date = int(re.sub(r"[a-zA-Z.]", "", entry.name))
+                    if date > self.active_file[0]:
+                        self.active_file = (date, entry)
+
+    def read_loop(self):
+        self.get_journal_file()
+        while getLauncher() and not self.stop:
+            if self.last_msg.get("event", None) != "Launcher":
+                self.send_message({"event": "Launcher", "timestamp": time.strftime(
+                    "%Y-%m-%dT%H:%M:%SZ", time.gmtime())})
+            self.new_file = False
+            while self.running and getGame():
+                if not self.new_file:
+                    self.send_message({"event": "GameStarted", "timestamp": time.strftime(
+                        "%Y-%m-%dT%H:%M:%SZ", time.gmtime())})
+                    time.sleep(10)
+                self.new_file = False
+                handle = win32file.CreateFile(
+                    self.active_file[1].path,
+                    win32file.GENERIC_READ,
+                    win32file.FILE_SHARE_DELETE | win32file.FILE_SHARE_READ | win32file.FILE_SHARE_WRITE,
+                    None,
+                    win32file.OPEN_EXISTING,
+                    0,
+                    None
+                )
+                detached_handle = handle.Detach()
+                file_descriptor = msvcrt.open_osfhandle(
+                    detached_handle, os.O_RDONLY)
+
+                with open(file_descriptor, encoding="utf-8") as journal:
+                    print(f"Reading {self.active_file[1]}")
+                    while not self.new_file:
+                        line = journal.readline()
+                        if line == "":
+                            time.sleep(0.1)
+                        else:
+                            event = json.loads(line)
+                            ev = event["event"]
+                            if ev == "Shutdown":
+                                self.new_file = True
+                                self.running = False
+                            elif ev == "Continued":
+                                self.new_file = True
+                            self.send_message(event)
+
+                        if self.stop:
+                            return
+
+                        self.recv_messages()
+
+                self.get_journal_file()
+            self.running = True
+            time.sleep(1)
+            self.recv_messages()
+
+            if self.stop:
+                return
+        self.send_message({"event": "GameShutdown", "timestamp": time.strftime(
+            "%Y-%m-%dT%H:%M:%SZ", time.gmtime())})
+
+    def recv_messages(self):
+        if self.con.poll():
+            msg = self.con.recv()
+            if msg == "closed":
+                self.stop = True
+
+    def send_message(self, msg: Dict):
+        self.con.send(msg)
+        self.last_msg = msg
+
+
+if __name__ == "__main__":
+    JournalFileApp()
---
+++ SettingsApp.py
@@ -0,0 +1,114 @@
+import os
+import re
+from kivy.clock import Clock
+import yaml
+
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import BooleanProperty, DictProperty
+from kivy.app import App
+
+from kivy.config import Config
+
+
+Config.set("graphics", "resizable", False)
+Config.set("graphics", "height", 600)
+Config.set("graphics", "width", 800)
+
+
+class ToggleOption(BoxLayout):
+    state = BooleanProperty(True)
+
+    def toggle_state(self):
+        self.state = not self.state
+
+    def get_setting(self):
+        return self.state
+
+
+class TextOption(BoxLayout):
+
+    def get_setting(self):
+        return self.input.text
+
+
+class Settings(Widget):
+    settings = DictProperty()
+
+    def __init__(self, con):
+        from kivy.core.window import Window
+        self.Window = Window
+        self.con = con
+        Clock.schedule_interval(self.check_queue, 0.5)
+        self.load_settings()
+        super().__init__()
+
+    def check_queue(self, dt):
+        if self.con.poll():
+            msg = self.con.recv()
+            if msg == "restore":
+                self.Window.minimize()
+                self.Window.restore()
+            elif msg == "closed":
+                App.get_running_app().stop()
+
+    def load_settings(self):
+        with open("settings.yaml", "r") as stream:
+            try:
+                data = yaml.safe_load(stream)
+                self.settings = data
+            except yaml.YAMLError as e:
+                print(e)
+
+    def convert_paths(self):
+        item = self.ids["elite_dangerous.path"].input_text
+        var = re.search("%(.+?)%", item)
+        if var is not None:
+            self.ids["elite_dangerous.path"].input_text = item.replace(
+                f"%{var.group(1)}%", os.environ[var.group(1)])
+
+        item = self.ids["general.journal_path"].input_text
+        var = re.search("%(.+?)%", item)
+        if var is not None:
+            self.ids["general.journal_path"].input_text = item.replace(
+                f"%{var.group(1)}%", os.environ[var.group(1)])
+
+    def save_settings(self):
+        self.convert_paths()
+        settings = dict()
+        for entry in self.ids:
+            cat, sett = entry.split(".")
+            if cat not in settings:
+                settings[cat] = dict()
+            settings[cat][sett] = self.ids[entry].get_setting()
+        with open("settings.yaml", "w") as stream:
+            try:
+                stream.write(yaml.dump(settings))
+            except yaml.YAMLError as e:
+                print(e)
+        self.con.send("changed_settings")
+
+
+class SettingsApp(App):
+    def __init__(self, con):
+        self.con = con
+        super().__init__()
+
+    def build(self):
+        self.title = "Elite Dangerous Rich Presence Settings"
+        return Settings(self.con)
+
+    @ staticmethod
+    def rgba(r, g, b, a):
+        return r / 255.0, g / 255.0, b / 255.0, a / 255.0
+
+    @ staticmethod
+    def hex(hex):
+        rgb = tuple((int(hex[1:][i:i + 2], 16) for i in (0, 2, 4)))
+        return SettingsApp.rgba(*rgb, 255)
+
+    def on_start(self):
+        pass
+
+    def on_stop(self):
+        pass
